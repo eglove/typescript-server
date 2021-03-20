8f3f9ff1798e0547b73e9acb8e642662e531503f
Ethan Glover <hello@ethang.email>
2021-03-20 16:27:39 -0500

    logins

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -20,5 +20,37 @@ exports.router.get('/login', (req, res) => {
 });
 exports.router.post('/login', (req, res) => {
     const { email, password } = req.body;
-    res.send(email + password);
+    if (email &&
+        password &&
+        email === 'hello@ethang.email' &&
+        password === '7N8CZ5YT3zfSW!') {
+        req.session = { loggedIn: true };
+        res.redirect('/');
+    }
+    else {
+        res.send('Invalid email or password.');
+    }
+});
+exports.router.get('/', (req, res) => {
+    var _a;
+    if ((_a = req.session) === null || _a === void 0 ? void 0 : _a.loggedIn) {
+        res.send(`
+      <div>
+        <div>You are logged in.</div>
+        <a href='/logout'>Logout</a>
+      </div>
+    `);
+    }
+    else {
+        res.send(`
+      <div>
+        <div>You are not logged in.</div>
+        <a href='/login'>Login</a>
+      </div>
+    `);
+    }
+});
+exports.router.get('/logout', (req, res) => {
+    req.session = undefined;
+    res.redirect('/');
 });
